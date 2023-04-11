e6b542439987e349a9e8b2c6763a403f13d91421
Alexandra Leschi <alexandra.leschi@hotmail.fr>
2023-04-11 13:13:32 +0200

    Update service model ajout

--- app/models/service.rb
+++ app/models/service.rb
@@ -8,35 +8,22 @@ class Service < ApplicationRecord
 
     data = Roo::Spreadsheet.open(file.path)
     headers = data.row(1) # get header row
-    
-    Service.all.each do |service| #on regarde si le service est dans le fichier
-      count = 0 
-      data.each_with_index do |row, idx|
-        next if idx == 0 # skip header
-          row_data = Hash[[headers, row].transpose]
-          if row_data['Nom'] == service.nom && row_data['Numero'].to_i == service.programme_id
-            count += 1
-          end 
-      end
-      if count == 0  #si nest pas dans le fichier on le supp ainsi que les mouvements associes
-        if Mouvement.where(service_id: service.id).count == 0
-          Mouvement.where(service_id: service.id).destroy_all
-          service.destroy
-        end
-      end
-    end
-
+    # @services_nom = data.column(1)
+    #Service.all.each do |service| #on regarde si le service est dans le fichier
+    #  unless @services_nom.include?(service.nom)
+    #    Mouvement.where(service_id: service.id).destroy_all
+    #    service.destroy
+    #  end
+    #end
     data.each_with_index do |row, idx|
       next if idx == 0 # skip header
       row_data = Hash[[headers, row].transpose]
-      if Programme.where('numero = ?',row_data['Numero'].to_i).count > 0
-        if Service.where('nom = ? AND programme_id = ?',row_data['Nom'].to_s,Programme.where('numero = ?',row_data['Numero'].to_i).first.id).count > 0 #service existe deja
-            @service = Service.where('nom = ?',row_data['Nom'].to_s).first
-        else
-            @service = Service.new 
-            @service.nom = row_data['Nom']
-            @service.programme_id = Programme.where('numero = ?',row_data['Numero'].to_i).first.id
-            @service.save
+      if Programme.where(numero: row_data['Numero'].to_i).count > 0
+        if Service.where(nom: row_data['Nom'].to_s, programme_id: Programme.where(numero: row_data['Numero'].to_i).first.id).count.zero?
+          @service = Service.new 
+          @service.nom = row_data['Nom']
+          @service.programme_id = Programme.where(numero: row_data['Numero'].to_i).first.id
+          @service.save
         end
       end
     end
